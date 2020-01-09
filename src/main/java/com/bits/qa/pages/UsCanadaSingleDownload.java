b560bf828fd9677a7fbac8c6ebc860420de9e5f6
rahul123pwr <rahul123pwr@gmail.com>
2020-01-09 19:53:50 +0530

    Code Optimization and Different plans for US

--- src/main/java/com/bits/qa/pages/UsCanadaSingleDownload.java
+++ src/main/java/com/bits/qa/pages/UsCanadaSingleDownload.java
@@ -64,127 +64,140 @@ public class UsCanadaSingleDownload extends TestBase{
 	@FindBy(xpath="//label[contains(text(),'Live')]")
 	WebElement freezeSld;
 	
+	// Plan name tab
+	@FindBy(xpath="//ul[@id=\"AddPlanTab\"]/li")
+	List<WebElement> plans;
+	
+	
+	
 	public UsCanadaSingleDownload()
 	{
 		PageFactory.initElements(driver, this);
 	}
 	
-	public boolean checkCommission(String User) 
+	public void submitUser(String User) 
 	{
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		
+
 		// Temp until dropdown auto select is resolved
 		String year = "2019";
 		String period = "80";
-		
-		
+
 		Select uSel = new Select(userDropdown);
 		uSel.selectByVisibleText(User);
-
 		wait.until(ExpectedConditions.attributeContains(loader, "class", "hidden"));
+		
 		Select ySel = new Select(yearDropdown);
 		ySel.selectByVisibleText(year);
-		
 		wait.until(ExpectedConditions.attributeContains(loader, "class", "hidden"));
 		Select pSel = new Select(periodDropdown);
 		pSel.selectByValue(period);
-		
 		wait.until(ExpectedConditions.attributeContains(loader, "class", "hidden"));
-		
-		
-		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+
+		sleep(1000);
 		if(!freezeBtn.isDisplayed())
 		{
 			freezeSld.click();
 		}
 		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-		
+		sleep(2000);
 		subbtn.click();
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		sleep(1000);
 		showMorebtn.click();
+	}
 
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Float Total = Float.parseFloat(incentiveTotal.getText().replaceAll(",", ""));
+	public float getCommFromSummary()
+	{
+		sleep(1000);
+		return Float.parseFloat(incentiveTotal.getText().replaceAll(",", ""));
+	}
 
+	public boolean checkTableExistance()
+	{
+		return CommissionTable.isDisplayed();
+	}
+	
+	public float commissionFor111(String User)
+	{
+		int numberOfRows = 3;
+		WebElement comm1 = driver.findElement(By.xpath("//table[@id=\"TableSlab\"]/tbody/tr["+numberOfRows+"]/td[3]"));
+		WebElement comm2 = driver.findElement(By.xpath("//table[@id=\"TableSlab\"]/tbody/tr["+numberOfRows+"]/td[4]"));
+		Float com1 = Float.parseFloat(comm1.getText().replaceAll(",", ""));
+		Float com2 = Float.parseFloat(comm2.getText().replaceAll(",", ""));
 		
-		// check if table exists or not
-		if(CommissionTable.isDisplayed())
+		jsScreenshot(User,comm2);
+		return com1+com2;
+	}
+	
+	public float commissionFor101(String User)
+	{
+		WebElement comm1 = driver.findElement(By.xpath("//*[@id=\"TblEffectivePer\"]/tbody/tr[1]/td[11]"));
+		WebElement comm2 = driver.findElement(By.xpath("//*[@id=\"TblEffectivePer\"]/tbody/tr[2]/td[11]"));
+		
+		Float com1 = Float.parseFloat(comm1.getText().replaceAll(",", ""));
+		Float com2 = Float.parseFloat(comm2.getText().replaceAll(",", ""));
+		
+		jsScreenshot(User,comm2);
+		return com1+com2;
+	}
+	
+	public float commissionForDefault(String User)
+	{
+		// dynamic table handling code STARTS here
+		int CommissionRow = rows.size();
+		WebElement comm1 = staticPath.findElement(By.xpath("//tr["+CommissionRow+"]/td[15]"));
+		WebElement comm2 = staticPath.findElement(By.xpath("//tr["+CommissionRow+"]/td[20]"));
+		// dynamic table handling code ENDS here
+		
+		Float com1 = Float.parseFloat(comm1.getText().replaceAll(",", ""));
+		Float com2 = Float.parseFloat(comm2.getText().replaceAll(",", ""));
+
+		jsScreenshot(User,comm2);
+		return com1+com2;
+	}
+	
+	
+	public boolean noTable(String User, Float Total)
+	{
+		jsScreenshot(User,incentiveTotal);
+		if(Total==0.0)
 		{
-			// dynamic table handling code STARTS here
-			int CommissionRow = rows.size();
-			WebElement comm1 = staticPath.findElement(By.xpath("//tr["+CommissionRow+"]/td[15]"));
-			WebElement comm2 = staticPath.findElement(By.xpath("//tr["+CommissionRow+"]/td[20]"));
-			// dynamic table handling code ENDS here
-			
-			Float com1 = Float.parseFloat(comm1.getText().replaceAll(",", ""));
-			Float com2 = Float.parseFloat(comm2.getText().replaceAll(",", ""));
-			
-			
-			// Code for Screenshot
-			JavascriptExecutor js = (JavascriptExecutor) driver;
-			js.executeScript("arguments[0].scrollIntoView();",comm2);
-			String SSName = "checkCommission - "+User;
-			ScreenShot.TakeFullPageScreenShot(driver,SSName,"USCASingle");
-			
-			Float CalculatedTotal = com1+com2;
-			
-			if(Math.floor(Total) == Math.floor(CalculatedTotal))
-			{
-				return true;
-			}
-			else
-			{
-				System.out.println("For user : "+User+",In summary total is "+Total+" but in table total is "+CalculatedTotal);
-				return false;
-			}
+			return true;
 		}
 		else
 		{
-			// Code for Screenshot
-			JavascriptExecutor js = (JavascriptExecutor) driver;
-			js.executeScript("arguments[0].scrollIntoView();",incentiveTotal);
-			String SSName = "checkCommission - "+User;
-			ScreenShot.TakeFullPageScreenShot(driver,SSName,"USCASingle");
-			if(Total==0.0)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
+			return false;
 		}
-		
-		
-		
 	}
-
-
+	
+	
+	public void jsScreenshot(String User, WebElement we)
+	{
+		// Code for Screenshot
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView();",we);
+		String SSName = "checkCommission - "+User;
+		ScreenShot.TakeFullPageScreenShot(driver,SSName,"USCASingle");
+	}
+	
+	public static void sleep(int ms)
+	{
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public int checkNoOfPlans()
+	{
+		return plans.size();
+	}
+	
+	public String planName(int i)
+	{
+		return (driver.findElement(By.xpath("//ul[@id=\"AddPlanTab\"]//li["+i+"]//a")).getText());
+	}
 
 }
--- src/test/java/com/bits/qa/testcases/UsCanadaSingleDownloadTest.java
+++ src/test/java/com/bits/qa/testcases/UsCanadaSingleDownloadTest.java
@@ -32,24 +32,49 @@ public class UsCanadaSingleDownloadTest extends TestBase{
 	}
 	
 	@Test(priority=1)
-	public void verifyCommission1() throws InterruptedException
+	public void verifyCommission1() 
 	{
-		Assert.assertTrue(USCommData.checkCommission("alan.mars"));	
+		String User="abby.hayden";
+		
+		USCommData.submitUser(User);
+		
+		// For users with single plan
+		if(USCommData.checkNoOfPlans()<2)
+		{
+			String planName = USCommData.planName(1);
+			
+			
+			Float Total = USCommData.getCommFromSummary();
+			Float CommissionInTable;
+			if(USCommData.checkTableExistance())
+			{
+				if(planName.equals("111"))
+				{
+					CommissionInTable = USCommData.commissionFor111(User);
+				}
+				else if(planName.equals("101"))
+				{
+					CommissionInTable = USCommData.commissionFor101(User);
+				}
+				else
+				{
+					CommissionInTable = USCommData.commissionForDefault(User);
+				}				
+				Assert.assertEquals(Math.floor(Total), Math.floor(CommissionInTable));
+			}
+			else
+			{
+				Assert.assertTrue(USCommData.noTable(User, Total));
+			}
+			
+		}
+		
+
+		
 	}
 	
 	
-	//@Test(priority=2)
-	public void verifyCommission2() throws InterruptedException
-	{
-		Assert.assertTrue(USCommData.checkCommission("alexander.liua"));	
-	}
-	
-	//@Test(priority=3)
-	public void verifyCommission3() throws InterruptedException
-	{
-		Assert.assertTrue(USCommData.checkCommission("celine.tracy"));	
-	}
-	
+
 	
 	
 	@AfterMethod
